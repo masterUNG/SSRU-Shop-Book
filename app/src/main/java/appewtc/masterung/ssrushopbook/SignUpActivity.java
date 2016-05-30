123c1e025d7818d6c37ebce6055f923c6b3cd16d
masterUNG <phrombutr@gmail.com>
2016-05-30 15:07:01 +0700

    Check Space

--- app/src/main/java/appewtc/masterung/ssrushopbook/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/ssrushopbook/SignUpActivity.java
@@ -1,13 +1,53 @@
 package appewtc.masterung.ssrushopbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, surnameEditText,
+            userEditText, passwordEditText;
+    private String nameString, surnameString,
+            userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+
+    }   // Main Method
+
+    public void clickSingUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
+
+        } else {
+            //No Space
+
+        }
+
+
+    }   // clickSign
+
+}   // Main Class
