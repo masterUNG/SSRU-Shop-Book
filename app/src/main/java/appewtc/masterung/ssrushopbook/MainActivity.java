037e07525f2d98a2e284c289015b34955bd5b71a
masterUNG <phrombutr@gmail.com>
2016-05-31 13:17:15 +0700

    Delete Data

--- app/src/main/java/appewtc/masterung/ssrushopbook/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ssrushopbook/MainActivity.java
@@ -1,6 +1,7 @@
 package appewtc.masterung.ssrushopbook;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,9 +20,20 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(MainActivity.this);
 
         //Test Add Value to SQLite
-        myManage.addNewUser("name", "sur", "user", "pass", "money");
+        //myManage.addNewUser("name", "sur", "user", "pass", "money");
+
+        //Delete All userTABLE
+        deleteAlluserTABLE();
 
     }   // Main Method
+
+    private void deleteAlluserTABLE() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+    }   // delete
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
